f29f6cc81880c7b3d43fd8128571f47133abe6e5
Wade Ren <wr@3dsafety.com.au>
2017-12-13 16:18:52 +1100

    apply material ui

--- src/app/core/components/company/contact-search/contact-search.component.ts
+++ src/app/core/components/company/contact-search/contact-search.component.ts
@@ -7,42 +7,7 @@ import { ContactFilter, Project } from '@app-core/models';
 @Component({
   selector: 'app-contact-search',
   templateUrl: './contact-search.component.html' ,
-  styles: [
-    `
-    md-card-title,
-    md-card-content,
-    md-card-footer {
-      display: flex;
-      justify-content: center;
-    }
-
-    md-card-footer {
-      color: #FF0000;
-      padding: 5px 0;
-    }
-
-    input {
-      width: 300px;
-    }
-
-    md-card-spinner {
-      padding-left: 60px; // Make room for the spinner
-    }
-
-    md-spinner {
-      width: 30px;
-      height: 30px;
-      position: relative;
-      top: 10px;
-      left: 10px;
-      opacity: 0.0;
-    }
-
-    md-spinner.show {
-      opacity: 1.0;
-    }
-  `,
-  ],
+  styles: ['./contact-search.component.css'],
 })
 export class ContactSearchComponent {
 
@@ -53,8 +18,10 @@ export class ContactSearchComponent {
   @Input() error = '';
   @Output() onSearch : EventEmitter<ContactFilter> = new EventEmitter<ContactFilter>();
   
-
-  triggerFilter(value) {
+  selectChange(){
+    alert(this.contactFilter.selectedProjectId)
+  }
+  triggerFilter() {
     this.onSearch.emit(this.contactFilter);
   }
 }
