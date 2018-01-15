938eae3066b66f6f4a6019eab4beef8db9403578
Wade Ren <wr@3dsafety.com.au>
2018-01-15 17:39:53 +1100

    seperate projectContractors from app folder

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,23 +1,26 @@
-import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
+// import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
 
-import * as fromUi from './reducers/ui-reducer';
+// import * as fromUi from './reducers/ui-reducer';
 
-export interface State {
-  ui: fromUi.UiState
-}
+// export interface State {
+//   ui: fromUi.UiState
+// }
 
-export const reducers: ActionReducerMap<State> = {
-  ui: fromUi.reducer
-  // more reducers here
-};
-
-
-/// selectors
-export const getUiState = createFeatureSelector<fromUi.UiState>('ui');
-
-export const getCurrentTitle = createSelector(getUiState, fromUi.getCurrentTitle);
+// export const reducers: ActionReducerMap<State> = {
+//   ui: fromUi.reducer
+//   // more reducers here
+// };
 
 
+// /// selectors
+// export const getUiState = createFeatureSelector<fromUi.UiState>('ui');
+
+// export const getCurrentTitle = createSelector(getUiState, fromUi.getCurrentTitle);
+
+
+export * from './reducers';
+export * from './actions';
+export * from './effects';
 
 
 
